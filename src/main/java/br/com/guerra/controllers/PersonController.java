8f3aef018b4f0f88e625b7217452691e6ac249b2
Guerra457 <matheus4xp@gmail.com>
2026-05-13 01:42:22 -0300

    DTO creation

--- src/main/java/br/com/guerra/controllers/PersonController.java
+++ src/main/java/br/com/guerra/controllers/PersonController.java
@@ -1,5 +1,6 @@
 package br.com.guerra.controllers;
 
+import br.com.guerra.data.dto.PersonDTO;
 import br.com.guerra.services.PersonService;
 import br.com.guerra.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,22 +18,22 @@ public class PersonController {
     private PersonService service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll() {
+    public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable("id") Long id) {
+    public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person) {
+    public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person) {
+    public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
 
