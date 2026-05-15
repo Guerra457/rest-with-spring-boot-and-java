a21e6ba78ed3ecf6d2a184fd94b17008b381a140
Guerra457 <matheus4xp@gmail.com>
2026-05-15 01:38:36 -0300

    creating migrations with flyway

--- src/main/java/br/com/guerra/controllers/TestLogController.java
+++ src/main/java/br/com/guerra/controllers/TestLogController.java
@@ -2,14 +2,16 @@ package br.com.guerra.controllers;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test/v1")
 public class TestLogController {
 
     private org.slf4j.Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
 
-    @GetMapping("/test")
+    @GetMapping
     public String testLog(){
         logger.debug("This is an DEBUG log");
         logger.info("This is an INFO log");
